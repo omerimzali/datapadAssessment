5a45762ceddda39fd23abe0d014ef8f1e195451c
Ömer Hayati <omerhayatiimzali@gmail.com>
2022-11-15 18:13:36 +0300

    customer Add Refund test created

--- services/customer.test.ts
+++ services/customer.test.ts
@@ -7,6 +7,14 @@ Deno.test("Brand Add Revenue", async () => {
   assertEquals(100, customer.getNetRevenue());
 });
 
+Deno.test("Brand Add Refund", async () => {
+    const customer = new Customer("Customer");
+    customer.addRevenue(100);
+    customer.addRefund(50);
+    assertEquals(50, customer.getNetRevenue());
+  });
+
+  
 Deno.test("Get Customer", async () => {
   const customer = new Customer("CustomerName");
   assertEquals("CustomerName", customer.getName());
