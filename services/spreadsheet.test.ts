2c5392a34b0e06318efa9bd3979643e467b2263d
Ömer Hayati <omerhayatiimzali@gmail.com>
2022-11-15 09:21:07 +0300

    spreadsheet.getAvgRevenueBrand method created.

--- services/spreadsheet.test.ts
+++ services/spreadsheet.test.ts
@@ -2,7 +2,7 @@ import { assertEquals } from "https://deno.land/std@0.164.0/testing/asserts.ts";
 import { Spreadsheet } from "./spreadsheet.ts";
 
 Deno.test("Getting Avg. Revenue By Brand", async () =>  {
-  const spreadsheet = new Spreadsheet({ type: "file", source: "file" });
+  const spreadsheet = new Spreadsheet({ type: "file", source: "data/test-data.csv" });
   const avgRevenueByBrand = await spreadsheet.getAvgRevenueBrand();
   assertEquals(100,avgRevenueByBrand["Brand1"]["value"]);
 });
--- services/spreadsheet.ts
+++ services/spreadsheet.ts
@@ -1,42 +1,95 @@
-
 import * as mod from "https://deno.land/std@0.164.0/collections/mod.ts";
+import { Brand } from "./brand.ts";
+import { parse as parseCsv } from "https://deno.land/std@0.82.0/encoding/csv.ts";
 
 export interface DataSourceOptions {
   type: "file" | "googlesheet";
   source: string;
 }
 
-
-
-
 export class Spreadsheet {
   dataType: string;
   source: string;
+  data?:any;
   constructor(
     options: DataSourceOptions = {
       type: "googlesheet",
       source: "goooglesheetURL",
     },
   ) {
+    console.log(options);
     this.dataType = options.type;
     this.source = options.source;
+
   }
 
-  public async getAvgRevenueBrand(){
+  private async readSpreadsheet() {
+    switch (this.dataType) {
+      case "googlesheet":
+        return await this.readGoogleSheet();
+        break;
+      case "file":
+        return await this.readFile();
+        break;
+    }
+  }
 
+  private async readGoogleSheet() {
+    /** @todo */
+    this.readFile();
+  }
+
+  private async readFile() {
+    const content = await parseCsv(
+      await Deno.readTextFileSync(this.source),
+      {
+        skipFirstRow: true,
+        columns: [
+          "evenTime",
+          "eventType",
+          "product",
+          "category",
+          "categoryCode",
+          "brand",
+          "price",
+          "user",
+          "session",
+        ],
+      },
+    );
+    return content;
+  }
+
+  public async getData() {
+    return this.data;
+  }
+
+  public async getAvgRevenueBrand() {
+    const result:any = {};
+    const brands:any = {};
+    this.data = await this.readSpreadsheet();
+
+    this.data.forEach((element) => {
+      if(element.eventType === 'purchase'){
+
+        if(!brands[element.brand]){
+          let brand = new Brand(element.brand);
+          brands[element.brand] = brand;
+        }
     
+        brands[element.brand].addPurchase(parseFloat(element.price));
+        result[element.brand] = {"value":brands[element.brand].getAvgRevenue()};
+      }
+    });
+    return result;
   }
 
-  public async getWeeklySessions(){
-  
+  public async getWeeklySessions() {
   }
 
-  public async getDailyConversion(){
-   
+  public async getDailyConversion() {
   }
 
-  public async getRevenueListOfCustomer(from_date: string, end_date: string){
-
+  public async getRevenueListOfCustomer(from_date: string, end_date: string) {
   }
-
 }
