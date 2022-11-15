3b886dafee0c47f3ecc95ff9197b3f94ca0c40a2
Ömer Hayati <omerhayatiimzali@gmail.com>
2022-11-15 08:50:03 +0300

    brand and conversion tests updated

--- services/brand.test.ts
+++ services/brand.test.ts
@@ -1,17 +1,14 @@
 import { assertEquals } from "https://deno.land/std@0.164.0/testing/asserts.ts";
 import { Brand } from "./brand.ts";
 
-Deno.test("Brand Avg Purchase", async () =>  {
+Deno.test("Brand Avg Purchase", async () => {
   const brand = new Brand("BrandName");
   brand.addPurchase(100);
-  assertEquals(100,brand.getAvgRevenue());
+  assertEquals(100, brand.getAvgRevenue());
 });
 
-
-
-Deno.test("Brand Get Name", async () =>  {
-    const brand = new Brand("BrandName");
-    brand.addPurchase(100);
-    assertEquals("BrandName",brand.getName());
-  });
-  
+Deno.test("Brand Get Name", async () => {
+  const brand = new Brand("BrandName");
+  brand.addPurchase(100);
+  assertEquals("BrandName", brand.getName());
+});
--- services/conversion.test.ts
+++ services/conversion.test.ts
@@ -1,36 +1,33 @@
 import { assertEquals } from "https://deno.land/std@0.164.0/testing/asserts.ts";
 import { Conversion } from "./conversion.ts";
 
-Deno.test("Conversion Purchased Number", async () =>  {
+Deno.test("Conversion Purchased Number", async () => {
   const conversion = new Conversion("Daily");
   conversion.addPurchase(100);
-  assertEquals(1,conversion.getPurchases());
+  assertEquals(1, conversion.getPurchases());
 });
 
-Deno.test("Conversion Revenue", async () =>  {
-    const conversion = new Conversion("Type");
-    conversion.addPurchase(100);
-    assertEquals(100,conversion.getRevenue());
-  });
+Deno.test("Conversion Revenue", async () => {
+  const conversion = new Conversion("Type");
+  conversion.addPurchase(100);
+  assertEquals(100, conversion.getRevenue());
+});
 
+Deno.test("Conversion Sessions", async () => {
+  const conversion = new Conversion("Daily");
+  conversion.addSession();
+  assertEquals(1, conversion.getSessions());
+});
 
-Deno.test("Conversion Sessions", async () =>  {
-    const conversion = new Conversion("Daily");
-    conversion.addSession();
-    assertEquals(1,conversion.getSessions());
-  });
-  
-  Deno.test("Conversion Sessions - Multiple", async () =>  {
-    const conversion = new Conversion("Daily");
-    conversion.addSession();
-    conversion.addSession();
-    assertEquals(2,conversion.getSessions());
-  });
-  
+Deno.test("Conversion Sessions - Multiple", async () => {
+  const conversion = new Conversion("Daily");
+  conversion.addSession();
+  conversion.addSession();
+  assertEquals(2, conversion.getSessions());
+});
 
-Deno.test("Conversion Type", async () =>  {
-    const conversion = new Conversion("Daily");
-    conversion.addPurchase(100);
-    assertEquals("Daily",conversion.getType());
-  });
-  
+Deno.test("Conversion Type", async () => {
+  const conversion = new Conversion("Daily");
+  conversion.addPurchase(100);
+  assertEquals("Daily", conversion.getType());
+});
