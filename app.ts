e3c23b37d6b6c8f01c6668ae1a606b9779ad4c2b
Ömer Hayati <omerhayatiimzali@gmail.com>
2022-11-17 00:26:04 +0300

    metrics.ts some imp

--- app.ts
+++ app.ts
@@ -1,15 +1,11 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-
+import { Router, Application, config } from './deps.ts';
 import { APP_HOST, APP_PORT } from "./config.ts";
 import router from "./routes.ts";
 
 
 const app = new Application();
 
-
 app.use(router.routes());
 app.use(router.allowedMethods());
-
-console.log(`Listening on port:${APP_PORT}...`);
 
 await app.listen(`${APP_HOST}:${APP_PORT}`);
--- controllers/metrics.ts
+++ controllers/metrics.ts
@@ -1,35 +1,40 @@
 import { Context, helpers } from "https://deno.land/x/oak/mod.ts";
 import { Spreadsheet } from "../services/spreadsheet.ts";
+import type { Metrics } from "../types/mod.ts";
 
 export const getMetrics = async (ctx: Context) => {
+
   const params = helpers.getQuery(ctx, { mergeParams: true });
-  const responseBody = {};
+
+  const { id, dimensions, aggregate } = params;
+  const responseBody:Metrics = {};
+  
   let values = {};
   const spreadsheet = await new Spreadsheet({
-    type: "googlesheet"
+    type: "googlesheet",
   });
 
   if (
-    params.id === "revenue" && params.dimensions === "brand" &&
-    params.aggregate === "avg"
+    id === "revenue" && dimensions === "brand" &&
+    aggregate === "avg"
   ) {
-    responseBody["values"] =  await spreadsheet.getAvgRevenueBrand();
+    responseBody["data"] = await spreadsheet.getAvgRevenueBrand();
   } else if (
-    params.id === "sessions" &&
-    params.dimensions === "date.weeknum" &&
-    params.aggregate === "distinct"
+    id === "sessions" &&
+    dimensions === "date.weeknum" &&
+    aggregate === "distinct"
   ) {
-    responseBody["values"]  = await spreadsheet.getWeeklySessions();
+    responseBody["values"] = await spreadsheet.getWeeklySessions();
   } else if (
-    params.id === "conversion" &&
-    params.dimensions === "date" &&
-    params.aggregate === "distinct"
+    id === "conversion" &&
+    dimensions === "date" &&
+    aggregate === "distinct"
   ) {
     responseBody["values"] = await spreadsheet.getDailyConversion();
   } else if (
-    params.id === "net-revenue" &&
-    params.dimensions === "customer" &&
-    params.aggregate === "sum"
+    id === "net-revenue" &&
+    dimensions === "customer" &&
+    aggregate === "sum"
   ) {
     responseBody["values"] = await spreadsheet.getRevenueListOfCustomer(
       params["filter.date.from"],
@@ -37,16 +42,15 @@ export const getMetrics = async (ctx: Context) => {
     );
   }
 
- 
-  responseBody["metric"] = params.id;
-  responseBody["dimensions"] = [params.dimensions];
-  responseBody["aggregation"] = params.aggregate;
+  responseBody["metric"] = id;
+  responseBody["dimensions"] = [dimensions];
+  responseBody["aggregation"] = aggregate;
   if (params["filter.date.from"]) {
     responseBody["date"] = {};
     responseBody["date"]["from"] = params["filter.date.from"];
     responseBody["date"]["to"] = params["filter.date.to"];
   }
 
-   ctx.response.body = responseBody;
-  return  ctx.response.body
+  ctx.response.body = responseBody;
+  return ctx.response.body;
 };
