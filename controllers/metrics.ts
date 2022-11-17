356ce88443ce72f51eb91b90456595a9aeb9457d
Ömer Hayati <omerhayatiimzali@gmail.com>
2022-11-17 06:17:46 +0300

    changed the way queryparrams are handled.

--- controllers/metrics.ts
+++ controllers/metrics.ts
@@ -3,54 +3,69 @@ import { Spreadsheet } from "../services/spreadsheet.ts";
 import type { Metrics } from "../types/mod.ts";
 
 export const getMetrics = async (ctx: Context) => {
-
   const params = helpers.getQuery(ctx, { mergeParams: true });
 
   const { id, dimensions, aggregate } = params;
-  const responseBody:Metrics = {};
+
+  const request = {
+    id,
+    dimensions,
+    aggregate,
+  };
+
+  const responseBody: Metrics = {};
 
   let values = {};
   const spreadsheet = await new Spreadsheet({
     type: "googlesheet",
   });
 
-  if (
-    id === "revenue" && dimensions === "brand" &&
-    aggregate === "avg"
-  ) {
-    responseBody.data = await spreadsheet.getAvgRevenueBrand();
-  } else if (
-    id === "sessions" &&
-    dimensions === "date.weeknum" &&
-    aggregate === "distinct"
-  ) {
-    responseBody["values"] = await spreadsheet.getWeeklySessions();
-  } else if (
-    id === "conversion" &&
-    dimensions === "date" &&
-    aggregate === "distinct"
-  ) {
-    responseBody.data = await spreadsheet.getDailyConversion();
-  } else if (
-    id === "net-revenue" &&
-    dimensions === "customer" &&
-    aggregate === "sum"
-  ) {
-    responseBody["values"] = await spreadsheet.getRevenueListOfCustomer(
+  const CASES = [{
+    id: "revenue",
+    dimensions: "brand",
+    aggregate: "avg",
+    func: spreadsheet.getAvgRevenueBrand(),
+  }, {
+    id: "sessions",
+    dimensions: "date.weeknum",
+    aggregate: "distinct",
+    func: spreadsheet.getWeeklySessions(),
+  }, {
+    id: "conversion",
+    dimensions: "date",
+    aggregate: "distinct",
+    func: spreadsheet.getDailyConversion(),
+  }, {
+    id: "net-revenue",
+    dimensions: "customer",
+    aggregate: "sum",
+    func: spreadsheet.getRevenueListOfCustomer(
       params["filter.date.from"],
       params["filter.date.to"],
-    );
-  }
+    ),
+  }];
 
   responseBody.metric = id;
   responseBody.dimensions = [dimensions];
   responseBody.aggregation = aggregate;
-  if (params["filter.date.from"]) {
 
+
+  if (params["filter.date.from"]) {
     responseBody["filter"]["date"] = {};
     responseBody["filter"]["date"]["from"] = params["filter.date.from"];
     responseBody["filter"]["date"]["to"] = params["filter.date.to"];
   }
+  
+  try {
+    responseBody.data = await CASES.find((CASE) =>
+      CASE.aggregate === request.aggregate &&
+      CASE.dimensions === request.dimensions && CASE.id === request.id
+    ).func;
+  } catch (e) {
+    return ctx.response.body;
+  }
+ 
+
 
   ctx.response.body = responseBody;
   return ctx.response.body;
