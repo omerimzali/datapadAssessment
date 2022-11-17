e7a3ad0044a66da83b3c0a09626fb8f056cdc480
Ömer Hayati <omerhayatiimzali@gmail.com>
2022-11-17 05:11:24 +0300

    Types added by lint's errors

--- types/mod.ts
+++ types/mod.ts
@@ -1,23 +1,52 @@
+
+import { Brand } from "../services/brand.ts";
+import { Conversion } from "../services/conversion.ts";
+import { Customer } from "../services/customer.ts";
 export type Metrics = {
   metric: "revenue" | "net-revenue" | "sessions" | "conversion";
   dimensions: string[];
   aggregation: "sum" | "distinct" | "avg";
-  filters?: { "date": { "from": string; "to": string }};
-  data: {[key: string]: { value: string; sessions?: string; purchases?: string }[]; };
+  filters?: { "date": { "from": string; "to": string } };
+  data: {
+    [key: string]: { value: string; sessions?: string; purchases?: string }[];
+  };
 };
 
 export type Row = {
-  eventTime: string
-  eventType: string
-  product: string
-  category: string
-  categoryCode: string
-  brand: string
-  price: string
-  user: string
-  session: string
-}
+  eventTime: string;
+  eventType: string;
+  product: string;
+  category: string;
+  categoryCode: string;
+  brand: string;
+  price: string;
+  user: string;
+  session: string;
+};
 
 export type RowList = [
-  Row
-]
+  Row,
+];
+
+export type ReturnData = {
+  [key: string]: { value: string; sessions?: string; purchases?: string };
+};
+export type DayList = {
+  [key: string]: Conversion;
+};
+
+export type WeekList = {
+  [key: string]: Conversion;
+};
+
+export type BrandList = {
+  [key: string]: Brand;
+};
+
+export type CustomerList = {
+  [key: string]: Customer;
+};
+
+export type SessionList = {
+  [key:string]: string
+} 
